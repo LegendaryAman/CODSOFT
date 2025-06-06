252d8d6a5789f1d4b3eb4ba1e1b5b3cc8920a9f7
LegendaryAman <161002501+LegendaryAman@users.noreply.github.com>
2025-06-06 17:09:04 +0530

    Fixed task and updated sone code

--- Task_1_Todo_list.py
+++ Task_1_Todo_list.py
@@ -1,44 +1,70 @@
 def ToDo_list():
-    list = []
-    print("!!! Welcome to the to do list !!!")
-    Num_of_list = int(input("Enter the number of list you want to add : "))
-    for x in range(1,Num_of_list+1):
-        print("list",x,":",end = '')
-        list_value = input("")
-        list.append(list_value)
-    print(list)
-    add = " "
-    update = 0
-    while True :
-        print(" 1. Add /t 2. Delete /t 3. Update /t 4.Delete all /t 5. View /t 6. exit")
-        value = int(input())
-        if(value == 1):
-            add = input("1. Add : ")
-            list.append(add)
-            print(f"Task {add} has succesfully added")      
-        elif(value == 2):
-            add = int(input("Enter the number of element to delete: "))
-            list.pop(add-1)
-            print(f"Task {add} has succesfully Deleted")
-        
-        elif(value == 3):
-            add = int(input("Enter the number of element to update : "))
-            update = input("Enter the element : ")
-            list.insert(add-1,update)
-            list.pop(add)
-            print(f"Task {add} has succesfully updated")
-        
-        elif(value == 4):
-            for x in range(0,len(list)):
-                list.pop(0)
-            print("all Task in the list has successfully deleted")
+    tasks = []
+    print("!!! Welcome to the To-Do List !!!")
+    try:
+        num = int(input("Enter the number of tasks you want to add: "))
+        for i in range(num):
+            task = input(f"Task {i+1}: ")
+            tasks.append(task)
+    except ValueError:
+        print("Please enter a valid number.")
+        return
 
-        elif(value == 5):
-            print(list)
+    print("\nYour list was successfully created!")
+    
+    while True:
+        print("\nChoose Operation:")
+        print("1. Add\t2. Delete\t3. Update\t4. Delete All\t5. View\t6. Exit")
+        try:
+            choice = int(input("Enter your choice: "))
+            if choice == 1:
+                new_task = input("Enter the new task to add: ")
+                tasks.append(new_task)
+                print(f"Task '{new_task}' added successfully.")
+            
+            elif choice == 2:
+                if not tasks:
+                    print("No tasks to delete.")
+                    continue
+                for i, t in enumerate(tasks, 1):
+                    print(f"{i}. {t}")
+                idx = int(input("Enter the task number to delete: "))
+                if 1 <= idx <= len(tasks):
+                    removed = tasks.pop(idx - 1)
+                    print(f"Task '{removed}' deleted successfully.")
+                else:
+                    print("Invalid task number.")
+            
+            elif choice == 3:
+                for i, t in enumerate(tasks, 1):
+                    print(f"{i}. {t}")
+                idx = int(input("Enter the task number to update: "))
+                if 1 <= idx <= len(tasks):
+                    updated_task = input("Enter the new task: ")
+                    tasks[idx - 1] = updated_task
+                    print("Task updated successfully.")
+                else:
+                    print("Invalid task number.")
+            
+            elif choice == 4:
+                tasks.clear()
+                print("All tasks deleted successfully.")
+            
+            elif choice == 5:
+                if tasks:
+                    print("\nYour To-Do List:")
+                    for i, task in enumerate(tasks, 1):
+                        print(f"{i}. {task}")
+                else:
+                    print("List is empty.")
+            
+            elif choice == 6:
+                print("Exiting... Goodbye!")
+                break
+            else:
+                print("Please enter a number between 1 and 6.")
+        
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
-        elif(value == 6):
-            break
-        else:
-            print("Please enter correct option")  
 ToDo_list()
-
